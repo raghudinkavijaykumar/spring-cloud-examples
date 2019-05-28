bc3af815fd2b3d53664ae15e6b19bcbbe52e749c
raghudv9 <42097006+raghudv9@users.noreply.github.com>
2019-05-28 11:32:03 +0530

    Add files via upload
    Spring cloud Eureka with two application servers and one client and discovery service.

---
+++ application-server/src/main/java/com/boeing/poc/applicationserver/ApplicationServerApplication.java
@@ -0,0 +1,28 @@
+package com.poc.applicationserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ApplicationServerApplication {
+
+	@Value("${server.instance.name}")
+	public String instance;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationServerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String message() {
+		return "Hello from "+ instance;
+	}
+
+}
